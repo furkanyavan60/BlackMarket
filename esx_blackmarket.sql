f3f117a83ea7cc5d36936840ac9befc13908fac9
ugurakay <50309526+ugurakay@users.noreply.github.com>
2019-08-12 18:06:29 +0300

    Update esx_blackmarket.sql

--- esx_blackmarket.sql
+++ esx_blackmarket.sql
@@ -22,4 +22,10 @@ CREATE TABLE IF NOT EXISTS `black_muhshipments` (
   `time` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+INSERT INTO `items` ( `name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('sarjorpistol', '9mm Sarjor', -1, 0, 1),
+('sarjortaramali', 'Taramalı Şarjör', -1, 0, 1),
+('sarjorroket', 'Roket', -1, 0, 1),
+('sarjorpompali', 'Pompalı Şarjör', -1, 0, 1),
+('sarjorkeskin', 'Keskin Nişancı Şarjör', -1, 0, 1),
+('sarjoragirtaramali', 'Ağır Taramalı Şarjör', -1, 0, 1);
